440195953e5ca771dcfada5d0d85b5078799eecf
Michael Obubelebra Amachree <70345027+Michael-Obele@users.noreply.github.com>
2022-05-11 16:05:08 +0100

    The Begining

---
+++ src/TimeTable.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function TimeTable() {
+  return (
+    <table border='1'>
+      <tbody>
+        <tr>
+          <td>Time</td>
+          <td className='text-center tl'>8:00 9:00</td>
+          <td className='text-center tl'>9:00 10:00</td>
+          <td className='text-center tl'>10:00 11:00</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
